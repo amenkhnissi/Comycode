a8c0978ff4218e6a306956916c918ba446ce1279
amenkh <amenkhnissi@gmail.com>
2021-07-31 07:36:53 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Form } from './Components/Form';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="main">
+   <Navbar />
+   <Form />
+   </div>
+  );
+}
+
+export default App;
